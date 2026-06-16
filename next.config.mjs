59967148ce316132c1ccf06fec9626d540007f52
Govardhan Reddy <govardhanreddyvardhan@gmail.com>
2026-06-16 09:51:54 +0530

    Configure GitHub Pages deployment via GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? '/Smartstudy-frontend' : '',
 };
 
 export default nextConfig;
+
